9d841538d69490ea933758213d91193bb7af554d
Hussein Esmail <hussein.esmail7@gmail.com>
2020-12-18 13:34:05 -0500

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,18 @@
+/*
+[PROGRAM NAME]
+[CREATOR]
+Created: [DATE]
+Updated: [DATE]
+Terminal command to run file:
+    g++ -o name filename.cpp && ./name
+Description: [DESCRIPTION]
+*/
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int nNumberOfArgs, char* pszArgs[]) {
+    // CODE HERE
+    return 0;
+}

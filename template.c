e4e574ab20664d3453539fd25b3796c8d81f93fe
Hussein Esmail <hussein.esmail7@gmail.com>
2021-02-04 18:37:51 -0500

    Add files via upload

---
+++ template.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+[PROGRAM NAME]
+[CREATOR]
+Created: [DATE]
+Updated: [DATE]
+Terminal command to run file:
+    gcc -o name filename.c && ./name
+Description: [DESCRIPTION]
+*/
+
+int main(void) {
+   // CODE HERE
+   
+   return 0;
+}

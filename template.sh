29d30789cd9db4f654852b509ffde07cdcb7f7e2
Hussein Esmail <hussein.esmail7@gmail.com>
2021-10-19 11:07:46 -0400

    modified file

---
+++ template.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# [FILE NAME]
+# [CREATOR]
+# Created: [DATE]
+# Updated: [DATE]
+# Description: [DESCRIPTION]
+
+# templateDescription: Bash Shell Script Document
+
+# CODE HERE
+
+exit 0

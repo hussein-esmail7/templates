e7250dbecb19950be85dde00005a9349e97e3c31
Hussein Esmail <hussein.esmail7@gmail.com>
2022-01-08 22:21:37 -0500

    Added colour variables

--- selenium-template.py
+++ selenium-template.py
@@ -13,27 +13,49 @@ import os
 import sys # To exit the program
 from selenium import webdriver
 from selenium.common.exceptions import *
+from webdriver_manager.chrome import ChromeDriverManager
 # from selenium.webdriver.support.ui import Select  # Used to select from drop down menus
-from selenium.webdriver.chrome.options import Options  # Used to add aditional settings (ex. run in background)
+from selenium.webdriver.chrome.service import Service # Used to set Chrome location
+from selenium.webdriver.chrome.options import Options # Used to add aditional settings (ex. run in background)
+from selenium.webdriver.common.by import By # Used to determine type to search for (normally By.XPATH)
 # from selenium.webdriver.common.keys import Keys  # Used for pressing special keys, like 'enter'
 
-bool_use_Brave = True
-bool_run_in_background = True
-chromedriver_path = "/Users/hussein/Downloads/chromedriver"
-target_site = "https://google.com"
+# ========= VARIABLES ===========
+bool_run_in_background  = True
+target_site             = "https://google.com"
+
+# ========= COLOR CODES =========
+color_end               = '\033[0m'
+color_darkgrey          = '\033[90m'
+color_red               = '\033[91m'
+color_green             = '\033[92m'
+color_yellow            = '\033[93m'
+color_blue              = '\033[94m'
+color_pink              = '\033[95m'
+color_cyan              = '\033[96m'
+color_white             = '\033[97m'
+color_grey              = '\033[98m'
+
+# ========= COLORED STRINGS =========
+str_prefix_q            = f"[{color_pink}Q{color_end}]"
+str_prefix_y_n          = f"[{color_pink}y/n{color_end}]"
+str_prefix_ques         = f"{str_prefix_q}\t "
+str_prefix_err          = f"[{color_red}ERROR{color_end}]\t "
+str_prefix_done         = f"[{color_green}DONE{color_end}]\t "
+str_prefix_info         = f"[{color_cyan}INFO{color_end}]\t "
 
 def main():
-    options = Options()  
+    options = Options()
     if bool_run_in_background:
         options.add_argument("--headless")  # Adds the argument that hides the window
-    if bool_use_Brave:
-        options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
-        driver = webdriver.Chrome(options=options)
-    else:
-        driver = webdriver.Chrome(chromedriver_path, options=options)
+    service = Service(ChromeDriverManager(log_level=0).install())
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.set_window_size(200, 1000) # Window size
     driver.get(target_site)
 
     # ADDITIONAL CODE HERE
+    # Example of getting elements by XPATH
+    # time_unformatted = driver.find_element(By.XPATH, '//time/span[1]').text + " /" + driver.                find_element(By.XPATH, '//time/span[2]').text¬
 
     # Cleanup
     driver.close()  # Close the browser
